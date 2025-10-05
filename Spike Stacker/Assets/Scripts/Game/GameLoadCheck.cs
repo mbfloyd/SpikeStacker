881a21e8a0db74699d57cfb5955b6ccdcc0ea98f
Michael Floyd <>
2025-10-04 18:54:18 -0700

    upgraded to unity 6.1

---
+++ Spike Stacker/Assets/Scripts/Game/GameLoadCheck.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoadCheck : MonoBehaviour
+{
+    void Awake()
+    {
+        try
+        {
+            IGameController gameController = DIContainer.Instance.Resolve<IGameController>();
+        }
+        catch
+        {
+            SceneManager.LoadScene("GameLoad");
+        }
+        Destroy(gameObject);
+    }
+}
